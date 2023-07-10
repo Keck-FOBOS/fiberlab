564265b9dfdc528820a6195ce291368fda50ba22
Kyle Westfall <westfall@ucolick.org>
2023-07-10 10:45:18 -0700

    edits to fullcone_farfield script

--- fiberlab/scripts/fullcone_farfield.py
+++ fiberlab/scripts/fullcone_farfield.py
@@ -14,7 +14,7 @@ class FullConeFarField(scriptbase.ScriptBase):
                                     width=width)
         parser.add_argument('img_file', default=None, type=str,
                             help='File with far-field output image from a collimated input')
-        parser.add_argument('-b', '--bkg_file', default=None, type=str,
+        parser.add_argument('--bkg_file', default=None, type=str,
                             help='File with only background flux')
         parser.add_argument('-p', '--pixelsize', default=0.018, type=float,
                             help='Size of the image camera pixels in mm.')
@@ -23,14 +23,33 @@ class FullConeFarField(scriptbase.ScriptBase):
         parser.add_argument('-t', '--threshold', default=1.5, type=float,
                             help='S/N threshold that sets the contour used to identify the center '
                                  'of the output ring.')
-        parser.add_argument('-w', '--window', default=False, action='store_true',
+        parser.add_argument('--show', default=False, action='store_true',
                             help='Display the QA plot in a window; do not write it to a file.')
+        parser.add_argument('--skip_plots', default=False, action='store_true',
+                            help='Skip the plot')
         parser.add_argument('-s', '--snr_img', default=False, action='store_true',
                             help='If creating the QA plot, show the estimated S/N of the data '
                                  'instead of the counts.')
-        parser.add_argument('-r', '--ring_box', default=None, type=float,
-                            help='Limit the plotted image regions to this times the best-fitting '
-                                 'peak of the ring flux distribution.  If None, the full image '
+        parser.add_argument('--smooth', default=False, action='store_true',
+                            help='Smooth the EE curve to limit interpolation errors')
+        parser.add_argument('-c', '--bkg_clip', nargs=3, default=[10., 100., 3.], type=float,
+                            help='Clipping parameters used for setting the contour threshold.  '
+                                 'Three numbers must be provided: (1) the number of clipping '
+                                 'iterations, (2) the lower rejection threshold, and (3) the '
+                                 'higher rejection threshold.')
+        parser.add_argument('-b', '--bkg_lim', nargs='+', default=None, type=float,
+                            help='One or multiple of the spot radius (as defined by the fitted '
+                                 'contour used to define spot center) to use for the background '
+                                 'estimate.  If none, the background is determined by first '
+                                 'clipping the high valued pixels.  If one number is provided, '
+                                 'all pixels above this radius are used.  If two numbers are '
+                                 'provided, pixels between the two radii are used.  No more than '
+                                 '2 values can be given!')
+        parser.add_argument('--bkg_sig', default=3., type=float,
+                            help='Sigma clipping level for the background measurement and masking.')
+        parser.add_argument('-w', '--window', default=None, type=float,
+                            help='Limit the plotted image regions to this times the fiducial '
+                                 'radius of the far-field spot. If None, the full image '
                                  'is shown.')
         return parser
 
@@ -43,6 +62,9 @@ class FullConeFarField(scriptbase.ScriptBase):
 
         from IPython import embed
 
+        import numpy
+        from scipy import interpolate
+
         from .. import fullcone
 
         img_file = Path(args.img_file).resolve()
@@ -53,25 +75,44 @@ class FullConeFarField(scriptbase.ScriptBase):
             raise FileNotFoundError(f'{bkg_file} does not exist!')
 
         # Analyze the image
-        plot_file = 'show' if args.window \
-                        else img_file.parent / f'{img_file.with_suffix("").name}_qa.png'
+        if args.skip_plots:
+            plot_file = None
+        elif args.show:
+            plot_file = 'show'
+        else:
+            img_file.parent / f'{img_file.with_suffix("").name}_qa.png'
         print(f'Analyzing {img_file.name}')
-        radius_img, img, radius, flux \
-                = fullcone.fullcone_farfield_output(img_file, bkg_file=bkg_file,
-                                                    threshold=args.threshold,
-                                                    pixelsize=args.pixelsize,
-                                                    distance=args.distance, plot_file=plot_file,
-                                                    snr_img=args.snr_img, ring_box=args.ring_box)
+        z0_ee = fullcone.fullcone_farfield_output(img_file, bkg_file=bkg_file,
+                                                  threshold=args.threshold,
+                                                  pixelsize=args.pixelsize, plot_file=plot_file,
+                                                  window=args.window, snr_img=args.snr_img, 
+                                                  clip_iter=int(args.bkg_clip[0]),
+                                                  sigma_lower=args.bkg_clip[1],
+                                                  sigma_upper=args.bkg_clip[2],
+                                                  bkg_lim=args.bkg_lim, bkg_lim_sig=args.bkg_sig)
 
-#        print('# Result from fobos_collimated_farfield script')
-#        print(f'# Written: {time.strftime("%a %d %b %Y %H:%M:%S",time.localtime())}')
-#        print(f'# Image file: {img_file.name}')
-#        print(f'# Pixelsize: {args.pixelsize} mm')
-#        if args.distance is not None:
-#            print(f'# Distance from fiber output to image: {args.distance:.2f} mm')
-#        print(f'# Ring radius:    {rad:.2f}')
-#        print(f'# Ring peak flux: {peak:.2f}')
-#        print(f'# Ring FWHM:      {fwhm:.2f}')
+        z0_ee_norm = z0_ee.ee/z0_ee.ee_norm
+        if args.smooth:
+            z0_ee_norm = contour.iterative_filter(z0_ee_norm, 301, 2)
+
+        z0_last = numpy.where(numpy.diff(z0_ee_norm) < 0)[0][0]
+        if args.bkg_lim is not None:
+            z0_last = min(z0_last, numpy.where(z0_ee.radius > args.bkg_lim[0]*z0_ee.circ_p[2])[0][0])
+#        print(z0_last, z0_ee.radius[z0_last]*args.pixelsize)
+
+        ee90 = interpolate.interp1d(z0_ee_norm[:z0_last],
+                                    z0_ee.radius[:z0_last]*args.pixelsize)(0.9)
+        fratio90 = None if args.distance is None else args.distance / 2 / ee90
+
+        print('# Result from fullcone_farfield script')
+        print(f'# Written: {time.strftime("%a %d %b %Y %H:%M:%S",time.localtime())}')
+        print(f'# Image file: {img_file.name}')
+        print(f'# Pixelsize: {args.pixelsize} mm')
+        print(f'# Total Flux: {z0_ee.ee_norm:.2f}')
+        print(f'# EE90: {ee90:.2f}')
+        if args.distance is not None:
+            print(f'# Distance from fiber output to image: {args.distance:.2f} mm')
+            print(f'# f/# at EE90: {fratio90:.2f}')
 
 
 
